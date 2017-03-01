0b5a94979bf0962c5be4242130bae22753e30d9d
Mark Weinreuter <mark@weinreuter.org>
2017-03-01 11:36:41 +0100

    Removed regular grid files. The gameboard will be a hex grid.

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 __author__ = 'Mark Weinreuter'
 import math
 
-board_thickness = 5
+board_thickness = 4
 
 # block dimensions
 block_r_mm = 20
-block_base_h_mm = 10
-block_hole_r_mm = 10
+block_base_h_mm = board_thickness *2
+block_hole_r_mm = 5
 block_hole_h_mm = board_thickness
+
+# log me in
+
+# viertel mm kleiner
 
 # distance between blocks
 block_pad_mm = 2
@@ -24,7 +28,7 @@ hex_side_len = math.sin(math.pi / 6) * 2 * block_r_pad_mm
 hex_side_len_half = hex_side_len / 2
 hex_height_pad_mm = math.sqrt(block_r_pad_mm ** 2 - hex_side_len_half ** 2)
 
-hex_outer_edge_pad = 4 + 2 * block_pad_mm  # ~ 4 is needed for no overlap
+hex_outer_edge_pad = 4 + 8 * block_pad_mm  # ~ 4 is needed for no overlap
 # Size of the hexagonal grid
 grid_r_mm = hex_block_count * hex_height_pad_mm + hex_outer_edge_pad
 
